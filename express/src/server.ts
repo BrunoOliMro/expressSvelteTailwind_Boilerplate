d5d3b09dd12c894f891d18f8aad9b4e04492f919
Bruno Oliveira <91101023+BrunoOliMro@users.noreply.github.com>
2023-01-09 13:51:07 -0300

    Commit

---
+++ express/src/server.ts
@@ -0,0 +1,25 @@
+import express from "express"
+
+import dotenv from "dotenv"
+
+import helmet from "helmet"
+
+import cors from "cors"
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
+app.use(helmet())
+app.use(express.json())
+
+const PORT = process.env[`PORT`]
+
+app.get("/teste", (_req, res) => {
+    res.send("Teste")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,svelte,js,css}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
